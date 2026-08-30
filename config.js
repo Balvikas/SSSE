769d5000a2fef8d8d35286a2c98b6220931260eb
Balvikas <pscdt009@gmail.com>
2026-08-30 14:13:59 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 🌍 GLOBAL CONFIGURATION MATRIX FOR BAL VIKAS ATTENDANCE
 // Update this URL ONCE here after a new deployment, and both pages update instantly!
 const GLOBAL_CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxy0_UpvUtqYaAaBqA20qL7q-7g9PHrmCexHQnKOa79NGlsUEhUMicwIGJxUKAqVdz6/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw-cIgma4ujxnQiHbY-HrBcoLxgVUBHP6MpznglbKyawyQsvrqZXNrfVFm2ISCqvGid/exec"
 };
